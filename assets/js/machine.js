74d57c62d34a6727d63350d3629f57e116dd7940
Pushkar Kadam <pushkarkadam17@outlook.com>
2022-06-16 11:00:26 +1000

    images and machine details added for epilog

---
+++ assets/js/machine.js
@@ -0,0 +1,8 @@
+import { API_ENDPOINT } from './config.js';
+import { apiData } from './utils.js';
+import { generateMachineTable } from './utils.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    let site = document.getElementById("location");
+    console.log(site.textContent);
+});
--- collections/_machines/EpilogFusion40.md
+++ collections/_machines/EpilogFusion40.md
@@ -2,4 +2,5 @@
 short_name: "laser"
 name: "Epilog Fusion 40"
 location: ["PEIH", "KWD"]
+image: "EpilogFusion40.jpg"
 ---
