b0ffdb4fd64ab22b85a51aaa959c020cfd0f651d
Pushkar Kadam <pushkarkadam17@outlook.com>
2022-06-16 11:40:41 +1000

    getting machine logs

--- assets/js/machine.js
+++ assets/js/machine.js
@@ -1,8 +1,45 @@
 import { API_ENDPOINT } from './config.js';
+import { sites } from './config.js';
 import { apiData } from './utils.js';
 import { generateMachineTable } from './utils.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-    let site = document.getElementById("location");
-    console.log(site.textContent);
+    // console.log(sites);
+
+    let machineName = document.getElementById("machineName").textContent;
+
+    let parser = new DOMParser();
+    let request = new XMLHttpRequest();
+    request.open("GET", API_ENDPOINT);
+    request.send();
+    request.onload = () => {
+        let xmlDoc = parser.parseFromString(request.response, "text/xml");
+
+        // Checking if there was an error
+        if (request.status != 200) {
+            console.log(`error ${request.status} ${request.statusText}`);
+        }
+
+        let machineData = apiData(xmlDoc);
+
+        console.log(machineData);
+
+        sites.forEach((site, index) => {
+            console.log(site);
+            machineData.forEach((campusData, index) => {
+                console.log(campusData["name"]);
+                if (site == campusData["name"]){
+                    let machines = campusData["equipment"];
+
+                    machines.forEach((machine, index) => {
+                        if (machineName == machine["equipid"]){
+                            let logs = machine["logs"];
+                            console.log(logs);
+                        }
+                    });
+                }
+            });
+        });
+    }
+
 });
